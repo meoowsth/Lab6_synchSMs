ffd3b85ae92ba105579d585aaf71a5dc565d8825
Tinghui Song <tinghuigz10@gmail.com>
2020-11-08 20:05:23 -0800

    added URL links

--- turnin/tsong008_lab6_part1.c
+++ turnin/tsong008_lab6_part1.c
@@ -7,7 +7,7 @@
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  *	
- *	Demo URL: 
+ *	Demo URL: https://drive.google.com/file/d/1vrw0aJGQXZz-jyosf2PG7bIA76OB2mWQ/view?usp=sharing
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
